b716d0848529e06060198c8b82649a41ae100659
arekso <arek.soulahian@gmail.com>
2017-12-28 13:32:30 -0800

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+//NAME: Arek Soulahian
+//DATE: 1-21-17
+//APP NAME: Hello World
+//PURPOSE: Print a statement
+
+class HelloWorld {
+   public static void main(String args[]) {
+      System.out.println("Hello World");
+   }//end main
+}//end class
